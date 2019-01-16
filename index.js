e04dfee6fc444db331cdd32e35a97f0b0ef20ca6
amirihoussem <houssem.amiri2@gmail.com>
2019-01-16 09:17:34 +0100

    updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{    
-fs.writeFile('customer-data.json',JSON.stringify(jsonObj), function(error){
+fs.writeFile('customer-data.json',JSON.stringify(jsonObj, null, 2), (error)=>{
 if (error) return console.log(error)
 console.log('writing is done correctly')
 })
