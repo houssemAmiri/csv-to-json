03d4a72a87243d8488cfb93975ca7fcda2c44846
amirihoussem <houssem.amiri2@gmail.com>
2019-01-16 08:51:23 +0100

    final commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const csvFilePath = './customer-data.csv'
+const csv = require('csvtojson')
+const fs = require('fs')
+
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{    
+fs.writeFile('customer-data.json',JSON.stringify(jsonObj), function(error){
+if (error) return console.log(error)
+console.log('writing is done correctly')
+})
+})
